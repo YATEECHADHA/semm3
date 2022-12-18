b0ae7ce1cda3a6d1c0f3610d957ea0f83c90b307
YATEECHADHA <104024680+YATEECHADHA@users.noreply.github.com>
2022-12-19 00:27:54 +0530

    Create pract11os.cpp

---
+++ pract11os.cpp
@@ -0,0 +1,55 @@
+11. Write a program to implement SRJF scheduling algorithm. 
+Source Code:
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+float avg_wt,avg_tat;
+cout<<"\nEnter number of process: ";
+cin>>n;
+cout<<"\nEnter Burst Time: ";
+for(i=0;i<n;i++)
+{
+cout<<"\nProcess "<<i+1<<": ";
+cin>>bt[i];
+p[i]=i+1; 
+}
+for(i=0;i<n;i++)
+{
+pos=i;
+for(j=i+1;j<n;j++)
+{
+if(bt[j]<bt[pos])
+pos=j;
+}
+temp=bt[i];
+bt[i]=bt[pos];
+bt[pos]=temp;
+temp=p[i];
+p[i]=p[pos];
+p[pos]=temp;
+}
+wt[0]=0; 
+for(i=1;i<n;i++)
+{
+wt[i]=0;
+for(j=0;j<i;j++)
+wt[i]+=bt[j];
+total+=wt[i];
+}
+avg_wt=(float)total/n; 
+total=0;
+cout<<"\nProcess   |\tBurst Time|Waiting Time|Turn Around Time";
+for(i=0;i<n;i++)
+{
+tat[i]=bt[i]+wt[i]; 
+total+=tat[i];
+cout<<"\nProcess: "<<p[i]<<"| \t"<<bt[i]<<"     | \t"<<wt[i]<<"      | \t"<<tat[i];
+}
+avg_tat=(float)total/n; 
+cout<<"\n\nAverage Waiting Time: "<<avg_wt;
+cout<<"\nAverage Turnaround Time: "<<avg_tat<<endl;
+return 0;
+}
